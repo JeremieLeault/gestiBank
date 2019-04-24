0d2d29b27124d40c1c0b7d63a1735888616753a3
JeremieLeault <49938455+JeremieLeault@users.noreply.github.com>
2019-04-24 17:28:47 +0200

    modif client
    Mise à jour du numéro de téléphone de number vers string

--- SB-Admin-BS4-Angular-6-master/src/app/admin/admin.module.ts
+++ SB-Admin-BS4-Angular-6-master/src/app/admin/admin.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DashboardAdminComponent } from './dashboard-admin/dashboard-admin.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [DashboardAdminComponent],
   imports: [
     CommonModule
   ]
--- SB-Admin-BS4-Angular-6-master/src/app/client/client.service.ts
+++ SB-Admin-BS4-Angular-6-master/src/app/client/client.service.ts
@@ -1,20 +1,20 @@
 import { Injectable } from '@angular/core';
-import { Client} from '../model/client';
+import { Client } from '../model/client';
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ClientService {
 
-  constructor() { }
+    constructor() { }
 
 
-  getAll()  {
-		const fakeClient = [
-		  new Client(0, "pwd", "Leault", "Jérémie", "jeremie.leault@gmail.com", 0706060606, "28 rue de GK", 69007, "Lyon", "celibataire", 0, 0 ),
-		  new Client(1, "stephane", "Indienne", "Ananthee", "ana@gmail.com", 0606060606, "8 rue GK", 69007, "Lyon", "celibataire", 0, 1)
-		]
-		return fakeClient;
-	
-	  }
+    getAll() {
+        const fakeClient = [
+            new Client(0, "pwd", "Leault", "Jeremie", "jeremie.leault@gmail.com", "0706060606", "28 rue de GK", 69007, "Lyon", "celibataire", 0, 0),
+            new Client(1, "stephane", "Indienne", "Ananthee", "ana@gmail.com", "0102030405", "8 rue GK", 69007, "Lyon", "celibataire", 0, 1)
+        ]
+        return fakeClient;
+
+    }
 
 }
