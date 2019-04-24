2091fa0e870e014bc8e5610502c25a02b2b1de3e
Lilian <niarrk@outlook.fr>
2019-04-24 17:34:45 +0200

    Branly_compile
    Jérémie est un magicien.

--- SB-Admin-BS4-Angular-6-master/src/app/client/client.service.ts
+++ SB-Admin-BS4-Angular-6-master/src/app/client/client.service.ts
@@ -10,8 +10,8 @@ export class ClientService {
 
   getAll()  {
 		const fakeClient = [
-		  new Client(0, "pwd", "Leault", "Jérémie", "jeremie.leault@gmail.com", 0706060606, "28 rue de GK", 69007, "Lyon", "celibataire", 0, 0 ),
-		  new Client(1, "stephane", "Indienne", "Ananthee", "ana@gmail.com", 0606060606, "8 rue GK", 69007, "Lyon", "celibataire", 0, 1)
+		  new Client(0, "pwd", "Leault", "Jeremie", "jeremie.leault@gmail.com", 606, "28 rue de GK", 69007, "Lyon", "celibataire", 0, 0 ),
+		  new Client(1, "stephane", "Indienne", "Ananthee", "ana@gmail.com", 60606, "8 rue GK", 69007, "Lyon", "celibataire", 0, 1)
 		]
 		return fakeClient;
 	
