fd1f836d7882ca6dce357175ca017cffe8904f20
JeremieLeault <49938455+JeremieLeault@users.noreply.github.com>
2019-04-25 15:25:24 +0200

    WARNING error à corriger

--- SB-Admin-BS4-Angular-6-master/src/app/manager/manager-routing.module.ts
+++ SB-Admin-BS4-Angular-6-master/src/app/manager/manager-routing.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ManagerComponent } from './manager.component';
+import { AjoutConseillerComponent } from './ajout-conseiller/ajout-conseiller.component';
+import { ListeConseillerComponent } from './liste-conseiller/liste-conseiller.component';
+import { EditConseillerComponent } from './edit-conseiller/edit-conseiller.component';
 
 const routes: Routes = [
     {
-        path: '',
-        component: ManagerComponent
+        path: '', component: ManagerComponent,
+
+        children: [
+
+            { path: 'liste-conseiller', component: ListeConseillerComponent },
+            { path: 'ajout-conseiller', component: AjoutConseillerComponent },
+            { path: 'edit-conseiller', component: EditConseillerComponent }
+        ],
     }
 ];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
+    declarations:
+        [ListeConseillerComponent,
+            AjoutConseillerComponent,
+            EditConseillerComponent
+        ],
     exports: [RouterModule]
 })
-export class ManagerRoutingModule {}
+export class ManagerRoutingModule { }
