ab271400e68775f074da2b8accdfde89f4e6ae15
Lilian <niarrk@outlook.fr>
2019-04-25 09:29:59 +0200

    Ajout client layout

--- SB-Admin-BS4-Angular-6-master/src/app/client/client.module.ts
+++ SB-Admin-BS4-Angular-6-master/src/app/client/client.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DashboardClientComponent } from './client-layout/dashboard-client/dashboard-client.component';
+import { ClientLayoutComponent } from './client-layout/client-layout.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [DashboardClientComponent, ClientLayoutComponent],
   imports: [
     CommonModule
   ]
