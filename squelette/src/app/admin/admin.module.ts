9d4c69411e66e828cb4e74b41771f858ba3982ce
JeremieLeault <49938455+JeremieLeault@users.noreply.github.com>
2019-05-05 16:35:54 +0200

    Navigation admin fini

--- squelette/src/app/admin/admin.module.ts
+++ squelette/src/app/admin/admin.module.ts
@@ -6,7 +6,7 @@ import { SearchAdminComponent } from './search-admin/search-admin.component';
 import { GestiConseillerComponent } from './gesti-conseiller/gesti-conseiller.component';
 import { GestiAffectComponent } from './gesti-affect/gesti-affect.component';
 import { MenuAdminComponent } from './menu-admin/menu-admin.component';
-import {RouterModule, Routes} from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { AjoutConseillerComponent } from './ajout-conseiller/ajout-conseiller.component';
 import { ListConseillerComponent } from './list-conseiller/list-conseiller.component';
 import { EditConseillerComponent } from './edit-conseiller/edit-conseiller.component';
@@ -14,40 +14,44 @@ import { FormsModule } from '@angular/forms';
 
 const adminRoutes: Routes = [
 
- {
-  path: '',
-  component: DashAdminComponent,
-   },
-     {
-      path: 'dash-admin/list-conseiller',
-      component: ListConseillerComponent
+    {
+        path: '',
+        component: DashAdminComponent,
+    },
+    //  {
+    //   path: 'dash-admin/list-conseiller',
+    //   component: ListConseillerComponent
+    // },
+    {
+        path: 'dash-admin/ajout-conseiller',
+        component: AjoutConseillerComponent
     },
     {
-      path: 'dash-admin/ajout-conseiller',
-      component: AjoutConseillerComponent
+        path: 'dash-admin/edit-conseiller',
+        component: EditConseillerComponent
     },
-    {
-      path: 'dash-admin/edit-conseiller',
-      component: EditConseillerComponent
-    },
-    {path: 'dash-admin/mail-admin', component: MailAdminComponent}
+    { path: 'dash-admin/mail-admin', component: MailAdminComponent },
+    { path: 'dash-admin/gesti-affect', component: GestiAffectComponent },
+    { path: 'dash-admin/gesti-conseiller', component: GestiConseillerComponent },
+    { path: 'dash-admin/search-admin', component: SearchAdminComponent }
+
 
 ];
 
 @NgModule({
-  declarations: [DashAdminComponent,
-    MailAdminComponent,
-    SearchAdminComponent,
-    GestiConseillerComponent,
-    GestiAffectComponent,
-    MenuAdminComponent,
-    AjoutConseillerComponent,
-    ListConseillerComponent,
-    EditConseillerComponent],
-  imports: [
-    CommonModule,
-    FormsModule,
-    RouterModule.forChild(adminRoutes)
-  ]
+    declarations: [DashAdminComponent,
+        MailAdminComponent,
+        SearchAdminComponent,
+        GestiConseillerComponent,
+        GestiAffectComponent,
+        MenuAdminComponent,
+        AjoutConseillerComponent,
+        ListConseillerComponent,
+        EditConseillerComponent],
+    imports: [
+        CommonModule,
+        FormsModule,
+        RouterModule.forChild(adminRoutes)
+    ]
 })
 export class AdminModule { }
