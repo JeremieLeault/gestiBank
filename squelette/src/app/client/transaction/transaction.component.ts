c020e87033dd8b9002ef9a8b7090292bad4020b3
tbranly <tbranly@gmail.com>
2019-05-07 18:37:09 +0200

    Page virement - MAJ liste compte et solde associé.

--- squelette/src/app/client/transaction/transaction.component.ts
+++ squelette/src/app/client/transaction/transaction.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { OnInit, Component } from '@angular/core';
+import { CompteService } from '../compte.service';
 
 @Component({
   selector: 'app-transaction',
   templateUrl: './transaction.component.html',
   styleUrls: ['./transaction.component.css']
 })
+
 export class TransactionComponent implements OnInit {
 
-  constructor() { }
+  listCompte;
+  constructor(private compteService: CompteService) { }
 
   ngOnInit() {
+    this.listCompte = this.compteService.getAll();
+     console.log(this.listCompte);
   }
 
 }
