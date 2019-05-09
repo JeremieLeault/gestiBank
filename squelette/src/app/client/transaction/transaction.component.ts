b66a9cb0562a820b2f9cb3144f90846a11a53d10
Lilian <niarrk@outlook.fr>
2019-05-09 16:50:24 +0200

    Modification transaction

--- squelette/src/app/client/transaction/transaction.component.ts
+++ squelette/src/app/client/transaction/transaction.component.ts
@@ -1,6 +1,8 @@
 import { OnInit, Component } from '@angular/core';
 import { CompteService } from '../compte.service';
 import { Compte } from '../compte';
+import { TransactionService } from './transaction.service';
+import { Retrait } from './retrait';
 
 @Component({
   selector: 'app-transaction',
@@ -13,8 +15,10 @@ export class TransactionComponent implements OnInit {
   listCompte;
   id:number;
   solde:Number;
+  montant:number;
+  libelle:string;
   tempCompte:Compte;
-  constructor(private compteService: CompteService) { }
+  constructor(private compteService: CompteService, private transactionService: TransactionService) { }
 
   ngOnInit() {
     this.listCompte = this.compteService.getAll();
@@ -24,5 +28,11 @@ export class TransactionComponent implements OnInit {
   getCompte($event){
     this.tempCompte = this.compteService.getCompte(this.id);
     this.solde = this.tempCompte.getSolde();
+  }
+
+  doTransaction(){
+    var retrait = new Retrait(this.id, this.montant, this.libelle, new Date());
+    this.transactionService.addRetrait(retrait);
+    console.log("Transaction efectué!");
   }
 }
